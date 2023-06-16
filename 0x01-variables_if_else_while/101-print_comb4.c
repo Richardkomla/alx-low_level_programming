eead10bf3f4a85fe840f40b3bb28ac2591142afc
Richardkomla <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@github.com>
2023-06-16 11:53:24 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
-int main() {
-    int i, j, k;
+	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 
-    for (i = 0; i <= 7; i++) {
-        for (j = i + 1; j <= 8; j++) {
-            for (k = j + 1; k <= 9; k++) {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-
-                if (i != 7 || j != 8 || k != 9) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-
-    putchar('\n');
-    return 0;
+				}
+				k++;
+			}
+			k = '0';
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
