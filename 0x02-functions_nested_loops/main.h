515c0c8634b735afbc0c0e23d68a60f980cced04
Richardkomla <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@github.com>
2023-06-20 13:43:27 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function Prototypes */
+
+/* Prints a single character */
+int _putchar(char c);
+
+/* Prints the alphabet in lowercase */
+void print_alphabet(void);
+
+/* Prints the alphabet in uppercase */
+void print_alphabet_uppercase(void);
+
+/* Prints a specified number of characters */
+void print_chars(int num_chars);
+
+/* Adds two integers and returns the result */
+int add_numbers(int a, int b);
+
+#endif /* MAIN_H */
