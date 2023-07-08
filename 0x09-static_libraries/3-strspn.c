df2f8c30f8ef0e4bec6c5ded181a3eb19d199a60
Richardkomla <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@github.com>
2023-07-07 18:44:06 -0700

    all

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,25 +1,33 @@
 #include "main.h"
 
 /**
- * _strspn - gets lengthof a prefix substring
- * @s: string to check
- * @accept: string to check against
- *
- * Return: number of bytes of s in accept
+ * _strspn - a function that gets the length of a prefix substring.
+ * @s: an input string
+ * @accept: an input character with to locate into string s
+ * Return: returns pointer to c position
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	int count = 0, flag;
+	char *start = accept;
 
-	for (i = 0; s[i]; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		flag = 0;
+		while (*accept)
 		{
-			if (s[i] == accept[j])
+			if (*accept == *s)
+			{
+				count++;
+				flag = 1;
 				break;
+			}
+			accept++;
 		}
-		if (!accept[j])
+		s++;
+		accept = start;
+		if (flag == 0)
 			break;
 	}
-	return (i);
+	return (count);
 }
