12f7f73612f0bbbcf7e460c15685d0e37bd296bd
Richardkomla <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@github.com>
2023-08-01 05:36:40 -0700

    again

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
-
+	
 	for (count = 0; h != NULL; count++)
-	{
+{
 		printf("%i\n", h->n);
 		h = h->next;
-	}
+}
 
 	return (count);
 }
