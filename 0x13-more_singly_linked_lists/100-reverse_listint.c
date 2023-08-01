e9f4acdeecabae80778b5b54c9f5cb6fb941fa75
Richardkomla <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@github.com>
2023-08-01 07:54:43 -0700

    hgt

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,19 +5,19 @@
  *
  * Return: pointer to head
  */
- listint_t *reverse_listint(listint_t **head)
+listint_t *reverse_listint(listint_t **head)
 {
-  listint_t *advent, *exit;
+	listint_t *advent, *exit;
 
-  advent = NULL;
-  while (*head !=NULL)
-    {
-      exit = (*head)->next;
-      (*head)->next = advent;
-      advent = *head;
-      *head = exit;
-    }
-  *head = advent;
+	advent = NULL;
+	while (*head != NULL)
+	{
+		exit = (*head)->next;
+		(*head)->next = advent;
+		advent = *head;
+		*head = exit;
+	}
+	*head = advent;
 
-  return = advent;
+	return (*head);
 }
